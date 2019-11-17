f07c73c7bdc77b290d3c5eacc8cb39a1a2a0c170
kutluToren <55382539+kutluToren@users.noreply.github.com>
2019-11-17 19:16:32 +0100

    Update test_calc_hw1.py
    Number of tests increased for testing operation selection

--- test_calc_hw1.py
+++ test_calc_hw1.py
@@ -34,11 +34,42 @@ def test_calc_double_divide():
     temp=calculator.divide(7,4.7)
     assert temp==round(float(1.48936170213),2)
     
+def test_calc_op_add():
+    temp=calculator.Calc('add',2.3,4.6)
+    assert temp==float(6.9)
+
+def test_calc_op_substract():
+    temp=calculator.Calc('subtract',4.73,2.21)
+    assert temp==float(2.52)
+    
+def test_calc_op_multiply():
+    temp=calculator.Calc('multiply',4.12,2.32)
+    assert temp==round(float(9.5584),2)
+    
+def test_calc_op_divide():
+    temp=calculator.Calc('divide',7,4.7)
+    assert temp==round(float(1.48936170213),2)
+    
+def test_calc_op_undef():
+    temp=calculator.Calc('noname',7,4.7)
+    assert temp=='Error'
+
+def test_calc_undef_mult():
+    temp=calculator.add('nonum','ahmet')
+    assert temp=='Error'
+
+def test_calc_undef_sub():
+    temp=calculator.subtract('nonum','ahmet')
+    assert temp=='Error'
+
+def test_calc_undef_mult():
+    temp=calculator.multiply('nonum','ahmet')
+    assert temp=='Error'
+    
+def test_calc_undef_div():
+    temp=calculator.divide('kemal','ahmet')
+    assert temp=='Error'
+    
 def test_calc_undef_divide():
     temp=calculator.divide(10,0)
     assert temp=='Error'
-    
-def test_calc_undef():
-    temp=calculator.divide(10,'ahmet')
-    assert temp=='Error'
-    
